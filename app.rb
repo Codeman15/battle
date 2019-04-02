d5bbd478a81d7d0bf632c4301d88c00371b9091e
Phil <phil.board90@googlemail.com>
2019-04-02 14:13:19 +0100

    added player sign up

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
+#  get '/' do
+#    'Testing infrastructure working!'
+#  end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+#  run! if app_file == $
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
 end
