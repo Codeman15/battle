18e81f178c14b18a77f495c7477cafc94e1557db
Phil <phil.board90@googlemail.com>
2019-04-03 12:05:45 +0100

    finished implementing hit points

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
 require 'sinatra/base'
+require './lib/player.rb'
 
 class Battle < Sinatra::Base
-#  get '/' do
-#    'Testing infrastructure working!'
-#  end
+  enable :sessions
 
-  # start the server if ruby file executed directly
-#  run! if app_file == $
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
+  end
+
+  get '/attack' do
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
+    erb :attack
   end
 end
