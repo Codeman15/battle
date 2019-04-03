e7c9483740b2379be59b12b569e9c71e37941faf
Phil <phil.board90@googlemail.com>
2019-04-03 16:01:46 +0100

    finished multiplayer

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -12,4 +12,22 @@ feature 'Attacking' do
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
+
+  scenario 'attack Player 1' do
+    sign_in_and_play
+    click_button 'attack'
+    click_button 'OK'
+    click_button 'attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'attack'
+    click_button 'OK'
+    click_button 'attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Dave: 60HP'
+    expect(page).to have_content 'Dave: 50HP'
+  end
 end
