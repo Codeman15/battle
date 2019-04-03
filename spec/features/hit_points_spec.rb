f06d455cee116065484f6f5e46622727c6ea4862
Phil <phil.board90@googlemail.com>
2019-04-03 14:27:18 +0100

    added player 1 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'view hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
   end
+
+  scenario 'see player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Dave: 60HP'
+  end
 end
