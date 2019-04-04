4c5bddc9e98707dc178d460317303f35da670369
Phil <phil.board90@googlemail.com>
2019-04-04 10:00:05 +0100

    winning and losing completed

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
-require './lib/player'
 require './lib/game'
+require './lib/player'
 require './lib/attack'
 
 class Battle < Sinatra::Base
@@ -22,15 +22,27 @@ class Battle < Sinatra::Base
     erb :play
   end
 
+  post '/attack' do
+    Attack.run($game.opponent_of($game.current_turn))
+    if $game.game_over?
+      redirect '/game-over'
+    else
+      redirect '/attack'
+    end
+  end
+
   get '/attack' do
     @game = $game
-    #@game.attack(@game.opponent_of(@game.current_turn))
-    Attack.run(@game.opponent_of(@game.current_turn))
     erb :attack
   end
 
   post '/switch-turns' do
     $game.switch_turns
     redirect('/play')
+  end
+
+  get '/game-over' do
+    @game = $game
+    erb :game_over
   end
 end
